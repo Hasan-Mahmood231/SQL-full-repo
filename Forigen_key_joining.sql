1720db3e38b8404073fef8692e0dcecaaf4c322b
Hasan-Mahmood231 <hk4145559@gmail.com>
2025-11-12 00:31:40 +0500

    update sql keys file

--- Forigen_key_joining.sql
+++ Forigen_key_joining.sql
@@ -1,5 +1,6 @@
  
 --select only customer who place order.
+--understand the forigen key and primery key.
 SELECT *
 FROM Sales.CustomerDetail as cd , Sales.Orders as ord
 where cd.CustomerID = ord.CustomerID
