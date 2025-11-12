815847e06332c2e8fe5450f6051f28de2517d7d1
Hasan-Mahmood231 <hk4145559@gmail.com>
2025-11-12 11:27:27 +0500

    add some topics

--- update_delete.sql
+++ update_delete.sql
@@ -11,6 +11,22 @@ VALUES
 ('se12923234','kalleem',234),
 ('se3423','khan',22)
 
+SELECT *
+FROM stud
+
+--update the student in data base 
+UPDATE stud 
+SET MARKS = 99
+WHERE reg = 'se3423'
+
+
+SELECT *
+FROM stud
+
+--now again insert the data of some students
+INSERT INTO stud(reg,namee,marks)
+VALUES
+('SE323232','hasseb',9909)
 
 SELECT *
 FROM stud
