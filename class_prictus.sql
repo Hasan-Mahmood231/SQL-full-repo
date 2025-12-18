ee7d7b39f6457c07638ab010b5bbee5c9d2ea08c
Hasan-Mahmood231 <hk4145559@gmail.com>
2025-12-18 14:18:27 +0800

    update and add homework to class work

--- class_prictus.sql
+++ class_prictus.sql
@@ -1,9 +1,5 @@
-CREATE DATABASE DB1;
-CREATE SCHEMA DB2;
 
-USE DB1;
-show TABLES;
-
+CREATE DATABASE T1	--create Database.
 CREATE TABLE T1(
 Reg VARCHAR(30) PRIMARY KEY NOT NULL,
 name VARCHAR(100) NOT NULL,
@@ -13,25 +9,31 @@ adress VARCHAR(150) DEFAULT 'NOT GIVEN'
 INSERT INTO t1 values 
 ( '23', 'ali' , 'xyz');
 
+--CHECK THE VALUES HERE.
 SELECT *
 FROM T1
 
-INSERT INTO Student (Reg) values('322')
+INSERT INTO T1(Reg) values('322')	--not inserted null bcz its NOTNULL.
+INSERT INTO T1(Reg, name) 
+values 
+	('45','aaa'),
+	('90','bb');
 
-INSERT INTO Student (Reg, name) 
-values  ('45','aaa'),
-		('90','bb');
+UPDATE T1 set name = 'hh' WHERE Reg = '45';
+DELETE FROM T1 WHERE Reg = '45'
 
-update Student set name = 'hh' WHERE Reg = '45';
-DELETE FROM Student WHERE Reg = '45'
-TRUNCATE TABLE STUDENT
-DROP TABLE Student
-DROP DATABASE DB1
 
-CREATE DATABASE T1
+
 INSERT INTO T1 
 VALUES 
 	('1','ABC','KOHAT'),
 	('2','BCD','PESHAWER')
-SELECT adress FROM T1
-CREATE TABLE T2  values(SELECT adress  FROM T1)
+
+
+--now make another table and insert the values from t1 to t2 new table.
+SELECT adress 
+INTO T2
+FROM T1
+
+SELECT *
+FROM T2
