9dab3e5dcc87cbeb46b3358f27bef6f6610c4292
Hasan-Mahmood231 <hk4145559@gmail.com>
2026-01-11 14:01:31 +0800

    update the file of lab task.

---
+++ lab_task.sql
@@ -0,0 +1,38 @@
+SELECT *
+FROM classicmodels.payments;
+SELECT *
+FROM classicmodels.orders;
+
+-- shiped order total amount?
+SELECT 
+O.status,
+sum(P.amount) AS TOTAL_AMOUNT
+FROM classicmodels.orders AS O
+JOIN classicmodels.payments AS P
+ON O.customerNumber = P.customerNumber
+GROUP BY O.status;
+
+SELECT 
+COUNT(comments)
+from classicmodels.orders
+where comments != 'NULL';
+
+SELECT 
+orderNumber,
+SUM(quantityOrdered * priceEach)
+FROM classicmodels.orderdetails
+GROUP BY orderNumber;
+
+SELECT *
+FROM classicmodels.products
+
+
+
+
+
+
+
+
+
+
+
